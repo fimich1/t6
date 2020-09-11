cf51d0cdbd68f786be85b74ae9dba2043e1834a3
fimich1 <38435351+fimich1@users.noreply.github.com>
2020-09-11 12:03:07 +0500

    добавлено окно подтверждения отправки, добавлен первый элемент списка

--- t6/lib/main.dart
+++ t6/lib/main.dart
@@ -1,12 +1,7 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:treinerId/athlets_form.dart';
 import 'web_matters.dart';
-
-//import 'package:english_words/english_words.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -30,47 +25,40 @@ class RandomWords extends StatefulWidget {
 class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
   var athlets;
-  final _saved = [];
-  // #enddocregion RWS-var
+  final _saved = ['кадеты'];
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final SnackBar snackBar = const SnackBar(content: Text('отправлены'));
 
-  // #docregion _buildSuggestions
   Widget _buildSuggestions() {
 // @f ну поехали, пытаемся всместо списка получить список с web
-
-//    FormController().getData();
+   int r=0;
+   int a=0;
     FormController().getFeedbackList().then((feedbackItems) {
       this.athlets = feedbackItems;
 
       setState(() {
-        this.athlets = feedbackItems;
+        //this.athlets = feedbackItems;
       });
     });
-//    var it = athlets.length;
-//    print("${athlets[2].id}");
-//    print('спортсменов с списке web = $it');
 
-    // @f ну поехали, пытаемся всместо списка получить список с web
-
+    
+    
+    athlets[0].name = 'Кадеты';
+    athlets[0].unit = '1';
+    athlets[0].id = '1';
+     
+    print ('длина списка равна $a');
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemCount: athlets.length,
-        itemBuilder: /*1*/ (context, i) {
-          //if (i.isOdd) return Divider(); /*2*/
-
-          final index = i; /*3*/
-//          if (index >= athlets.length) {
-//            athlets.addAll;
-//          } /*4*/
-//
-//            //           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-//          }
-          String row = athlets[index].name;
-          return _buildRow(row);
-        });
+      padding: const EdgeInsets.all(16.0),
+      itemCount: athlets.length,
+      itemBuilder: (context, i) {
+        final index = i;
+        String row = athlets[index].name;
+        return _buildRow(row);
+      },
+    );
   }
-  // #enddocregion _buildSuggestions
 
-  // #docregion _buildRow
   Widget _buildRow(pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -93,9 +81,7 @@ class RandomWordsState extends State<RandomWords> {
       },
     );
   }
-  // #enddocregion _buildRow
 
-  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,7 +98,28 @@ class RandomWordsState extends State<RandomWords> {
       body: _buildSuggestions(),
     );
   }
-  // #enddocregion RWS-build
+
+void _showCupertinoDialog() {
+    showDialog(
+        context: context,
+        builder: (_) => new CupertinoAlertDialog(
+              title: new Text("Список присутствующих на тренировке"),
+              content: new Text("отправлен"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Принято!'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ));
+  }
+
+
+
+
+
 
   void _pushSaved() {
     Navigator.of(context).push(
@@ -136,13 +143,7 @@ class RandomWordsState extends State<RandomWords> {
           ).toList();
 
           return Scaffold(
-//            bottomNavigationBar: BottomAppBar(
-//              color: Colors.pink,
-//              child: Text(
-//                'отправить',
-//                style: _biggerFont,
-//              ),
-//            ),
+            //       key: scaffoldKey,
             appBar: AppBar(
               actions: <Widget>[
                 IconButton(
@@ -150,6 +151,7 @@ class RandomWordsState extends State<RandomWords> {
                   tooltip: 'Отправить всех в облако',
                   onPressed: () {
                     submit(_saved);
+                    _showCupertinoDialog();
                   },
                 ),
               ],
@@ -161,44 +163,33 @@ class RandomWordsState extends State<RandomWords> {
       ),
     );
   }
-// #docregion RWS-var
 }
 
-// #enddocregion RWS-var
-//final _formKey = GlobalKey<FormState>();
-//final _scaffoldKey = GlobalKey<ScaffoldState>();
-//// Method to show snackbar with 'message'.
-//_showSnackbar(String message) {
-//  final snackBar = SnackBar(content: Text(message));
-//  _scaffoldKey.currentState.showSnackBar(snackBar);
-//}
-
 // функция отправки одной строки
-void submit(saved) {
-  //print(_saved[1]);
-  // Validate returns true if the form is valid, or false
-  // otherwise.
+void submit(List saved) {
+//  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+//  final SnackBar snackBar = const SnackBar(content: Text('отправлены'));
 
-  // If the form is valid, proceed.
+  int i = saved.length;
+  print('список тренирующихся = $i');
+
   Today today = Today(saved[0]);
 
   FormController formController = FormController();
-
-  //_showSnackbar("Данные отправляюся");
 
   // Submit 'feedbackForm' and save it in Google Sheets.
   formController.submitForm(today, (String response) {
     print("Response: $response");
     if (response == FormController.STATUS_SUCCESS) {
+      //   scaffoldKey.currentState.showSnackBar(snackBar);
+     
       // Feedback is saved succesfully in Google Sheets.
       print('1 спортсмен отправлен');
-      //_showSnackbar("Список отправлен");
     } else {
-      // Error Occurred while saving data in Google Sheets.
-      // _showSnackbar("Что-то не так!");
       print("Что-то не так!");
     }
   });
 }
+
 
 void main() => runApp(MyApp());
